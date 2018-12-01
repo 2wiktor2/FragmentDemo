102b044d2d80b8da3eade1e337de7dcb7f1a5de2
Wiktor22 <wiktor@bk.ru>
2018-12-01 20:41:58 +0300

    Ъ работает

--- app/src/main/java/com/wiktor/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/wiktor/fragmentdemo/MainActivity.java
@@ -1,13 +1,27 @@
 package com.wiktor.fragmentdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends FragmentActivity {
-
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
+Button buttonToNextActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonToNextActivity = findViewById(R.id.button_to_next_activity);
+
+        buttonToNextActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent1 = new Intent(this, SecondActivity.class);
+        startActivity(intent1);
+
     }
 }
---
+++ app/src/main/java/com/wiktor/fragmentdemo/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.wiktor.fragmentdemo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class SecondActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
